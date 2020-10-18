7726d9dc1fe876112d45d53e0beab157983754ae
Tom Magin <magintc@mac.com>
2020-10-18 19:19:51 +1100

    #still setting up

--- src/server/index.js
+++ src/server/index.js
@@ -2,3 +2,44 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 console.log("API Key", process.env.API_KEY);
+console.log("PORT", process.env.PORT);
+const PORT = process.env.PORT;
+const API_Key = process.env.API_KEY;
+const path = require("path");
+const mockAPIResponse = require("./mockAPI");
+const fetch = require("node-fetch");
+const express = require("express");
+const app = express();
+
+const URL = "https://api.meaningcloud.com/sentiment-2.1?key=";
+
+const cors = require("cors");
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.text());
+
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile("dist/index.html");
+});
+
+app.get("/test", (req, res) => {
+  res.send(mockAPIResponse);
+});
+
+app.post('document',(req,res)=>{
+const res = await fetch(`${URL}${API_Key}&lang=en&url=${req.body}`);
+try{
+    const data = await res.json();
+    console.log(data);
+    res.send(data);
+}catch(err){
+    console.log(err);
+}
+});
+
+app.listen(PORT, ()=>{
+console.log(`Listening on ${PORT}`);
+});
