b2d5bcc387013793b3a84d2bd21e14ba4606cfbc
Tom Magin <magintc@mac.com>
2020-10-18 18:57:56 +1100

    #adding webpack, dependencies and setting up files

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { urlChecker } from "./js/urlChecker";
+import { formHandler } from "./js/formHandler";
+
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+import "./styles/resets.scss";
+console.log(urlChecker);
+console.log(formHandler);
+
+export { urlChecker, formHandler };
