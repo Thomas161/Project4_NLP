7dbb3b34e2ec14a1b6ef042877b13b57158784b9
Tom Magin <magintc@mac.com>
2020-10-26 14:56:50 +1100

    #creating more test cases for urlChecker

--- __test__/urlChecker.test.js
+++ __test__/urlChecker.test.js
@@ -1,6 +1,23 @@
 import { checkTheUrl } from "../src/client/js/urlChecker";
 
-test("Test", () => {
-  expect(checkTheUrl).toBeDefined();
+describe("Check function defined and that its truthy", () => {
+  test("function is defined ", () => {
+    expect(checkTheUrl).toBeDefined();
+  });
+  test("function to be truthy", () => {
+    expect(checkTheUrl).toBeTruthy();
+  });
 });
-console.log("test", checkTheUrl);
+
+describe("url protocol is attached", () => {
+  test("returns true for unsecured protocol ", () => {
+    const inputHttp = "http://www.facebook.com";
+    expect(checkTheUrl(inputHttp)).toBe(true);
+  });
+  test("returns true for secure protocol", () => {
+    const inputHttps = "https://github.com";
+    expect(checkTheUrl(inputHttps)).toBe(true);
+  });
+});
+
+// console.log("test", checkTheUrl);
